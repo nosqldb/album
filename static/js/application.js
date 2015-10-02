a3ce8973c51fe33ac4321ff07583527162a4f787
duoyun <occj@qq.com>
2015-10-02 09:53:31 +0800

    change editor style

--- static/js/application.js
+++ static/js/application.js
@@ -19,27 +19,28 @@ function setToTop() {
 function createEditorMd(divId, submitId, markdown) {
     var editor = editormd(divId, {
         height: 400,
-		markdown: markdown,
-	    autoFocus: false,
+        markdown: markdown,
+        autoFocus: false,
+        watch : false, 
         path: "http://77fkk5.com1.z0.glb.clouddn.com/static/lib/editor.md-1.5.0/lib/",
-	    placeholder: "Mardkown，提交前请查看预览格式是否正确",
+        placeholder: "Markdown，提交前请查看预览格式是否正确",
         toolbarIcons: function() {
-          return ["undo", "redo", "|", "bold", "italic", "quote", "|", "h1", "h2", "h3", "h4", "h5", "h6", "|", "list-ul", "list-ol", "hr", "|", "link", "reference-link", "image", "code", "preformatted-text", "code-block", "|", "goto-line", "watch", "preview", "fullscreen", "|", "help", "info"]
+          return ["undo", "redo", "|", "bold", "italic", "quote", "|", "h1", "h2", "h3", "|", "list-ul", "list-ol", "hr", "|", "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "|", "watch", "preview", "fullscreen", "|", "help", "info"]
         },
         saveHTMLToTextarea: true,
         imageUpload: true,
         imageFormats: ["jpg", "jpeg", "gif", "png"],
         imageUploadURL: "/upload/image",
-	    onchange: function() {
-	      $(submitId).attr('disabled', this.getMarkdown().trim() == "");
-	    }
+        onchange: function() {
+          $(submitId).attr('disabled', this.getMarkdown().trim() == "");
+        }
       });
 
-	return editor;
+    return editor;
 }
 
 $(document).ready(function(){
-	editormd.urls.atLinkBase = "/user/";
+    editormd.urls.atLinkBase = "/user/";
 
     $("[data-toggle=popover]").popover();
 
